27937d00d691370e5ef8b6ea344ac1363f24e08d
asaurasieu <asauras.ieu2021@student.ie.edu>
2024-10-27 22:17:02 +0100

    fix code error

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def upload_image():
 if __name__ == '__main__': 
     if not os.path.exists('temp'):
         os.makedirs('temp')
-    app.run(debug=True, host='0.0.0.0' port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
